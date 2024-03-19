5c8ab52e43bed7b945957e2081739b902cd44703
airu411 <airu411git@gmail.com>
2024-03-19 10:25:07 +0800

    added carbonell

---
+++ src/components/navbar/Nav.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+import "./navstyles.css"
+
+function Nav() {
+  return (
+    <div>
+      
+        <nav className="nav">
+
+            <div className="left">
+                <ul>
+                    <li>
+                        <Link to="/Carbonell" className="link">Carbonell</Link>
+                    </li>
+
+                    <li> {/* Change to your surname  */}
+                        <Link to="/effect" className="link">Aguas</Link>
+                    </li>
+
+
+                    <li>
+                        <Link to="/map" className="link">Del Ayre</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/hooks" className="link">Baguio</Link>
+                    </li>
+
+                    <li>
+                        <Link to="/hooks" className="link">Bersabal</Link>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/components/pages/Carbonell/Carbonell.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// Change to your surname
+import "./Carbonell.css"
+
+// Change to your surname
+function Carbonell() {
+
+  return (
+    <div className="content">
+      
+      {/* Change to your name and student number */}
+      <p>Carbonell, Angela Gabrielle V. SN: 2021104379</p>
+
+    </div>
+  )
+}
+
+// Change to your surname
+export default Carbonell
