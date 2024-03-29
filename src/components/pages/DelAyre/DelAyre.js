cd02acc438fcf37e650af309e5b6ae530026d505
WTFRaph <wtfraph@gmail.com>
2024-03-29 20:13:14 +0800

    added <DelAyre>

---
+++ src/components/pages/DelAyre/DelAyre.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// Change to your surname
+import "./DelAyre.css"
+
+// Change to your surname
+function DelAyre() {
+
+  return (
+    <div className="content">
+      
+      {/* Change to your name and student number */}
+      <p>Del Ayre, Raphaelo D. SN: 2021102349</p>
+
+    </div>
+  )
+}
+
+// Change to your surname
+export default DelAyre
